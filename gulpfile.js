10f0cd31798cdb0bcbeffc40f68bd82c7540c6e0
Jeff Posnick <jeffy@google.com>
2017-05-21 22:39:02 -0400

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const babel = require('gulp-babel');
 const concat = require('gulp-concat-util');
 const cssnano = require('gulp-cssnano');
 const foreach = require('gulp-foreach');
@@ -7,6 +8,7 @@ const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const sequence = require('run-sequence');
 const serve = require('serve');
+const uglify = require('gulp-uglify');
 
 const SRC_DIR = 'src';
 const BUILD_DIR = 'build';
@@ -15,28 +17,36 @@ gulp.task('clean', () => {
   return fse.emptyDir(BUILD_DIR);
 });
 
+const CSS_SRC = `${SRC_DIR}/css/**/*`;
 gulp.task('css', () => {
-  return gulp.src(`${SRC_DIR}/css/**/*`)
+  return gulp.src(CSS_SRC)
     .pipe(cssnano())
     .pipe(gulp.dest(`${BUILD_DIR}/css`));
 });
 
+const JS_SRC = `${SRC_DIR}/js/**/*`;
 gulp.task('js', () => {
-  return gulp.src(`${SRC_DIR}/js/**/*`)
+  return gulp.src(JS_SRC)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
     .pipe(gulp.dest(`${BUILD_DIR}/js`));
 });
 
+const IMAGES_SRC = `${SRC_DIR}/images/**/*`;
 gulp.task('images', () => {
-  return gulp.src(`${SRC_DIR}/images/**/*`)
+  return gulp.src(IMAGES_SRC)
     .pipe(gulp.dest(`${BUILD_DIR}/images`));
 });
 
+const FONTS_SRC = `${SRC_DIR}/fonts/**/*`;
 gulp.task('fonts', () => {
-  return gulp.src(`${SRC_DIR}/fonts/**/*`)
+  return gulp.src(FONTS_SRC)
     .pipe(gulp.dest(`${BUILD_DIR}/fonts`));
 });
 
-
+const HTML_SOURCE = `${SRC_DIR}/partials/**/*.html`;
 gulp.task('html:concat', async () => {
   const header = await fse.readFile(`${SRC_DIR}/partials/common/header.html`);
   const footer = await fse.readFile(`${SRC_DIR}/partials/common/footer.html`);
@@ -51,7 +61,7 @@ gulp.task('html:concat', async () => {
 });
 
 gulp.task('html:partials', async () => {
-  return gulp.src(`${SRC_DIR}/partials/**/*.html`)
+  return gulp.src(HTML_SOURCE)
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest(`${BUILD_DIR}/partials`));
 });
@@ -62,6 +72,14 @@ gulp.task('build', callback => {
     ['css', 'js', 'images', 'fonts', 'html:concat', 'html:partials'],
     callback
   );
+});
+
+gulp.task('watch', ['build'], callback => {
+  gulp.watch(CSS_SRC, ['css']);
+  gulp.watch(JS_SRC, ['js']);
+  gulp.watch(IMAGES_SRC, ['images']);
+  gulp.watch(FONTS_SRC, ['fonts']);
+  gulp.watch(HTML_SOURCE, ['html:concat', 'html:partials']);
 });
 
 gulp.task('serve', callback => {
---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+  for (const a of document.querySelectorAll('nav > ul > li > a')) {
+    if (a.href === location.href) {
+      a.classList.add('active');
+    } else {
+      a.classList.remove('active');
+    }
+  }
+
+  if ('serviceWorker' in navigator) {
+
+  }
+});
