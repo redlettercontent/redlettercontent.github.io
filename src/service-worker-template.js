59661d83b6eb5267c83557300ed8d10d08f67ac0
Jeff Posnick <jeffy@google.com>
2017-09-21 21:27:46 -0400

    Updated the unpkg URLs.

--- src/service-worker-template.js
+++ src/service-worker-template.js
@@ -1,6 +1,6 @@
 importScripts(
-  'https://unpkg.com/workbox-sw@1.0.0',
-  'https://unpkg.com/workbox-google-analytics@1.0.0'
+  'https://unpkg.com/workbox-sw@2.0.1/build/importScripts/workbox-sw.prod.v2.0.1.js',
+  'https://unpkg.com/workbox-google-analytics@2.0.0/build/importScripts/workbox-google-analytics.prod.v2.0.0.js'
 );
 
 const workboxSW = new WorkboxSW({clientsClaim: true, skipWaiting: true});
